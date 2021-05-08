9b4edd9ad31e528d19f5f96d8a5f92aaea5fe285
ritpanagiotis <ritpanagiotis@gmail.com>
2021-05-08 14:50:24 +0200

    added outlier removal according to std and mean,and calculation of new max dimensions for further padding

--- data_investigation.py
+++ data_investigation.py
@@ -105,16 +105,12 @@ def data_size_stats(data_path):
 
 def CroppedCharAnalysis(data_path):
     data = glob.glob(data_path)
-    #use to find std, mean of newly cropped images.
     img_size_all_x = 0
     img_size_all_y = 0
-    std_y = 0
-    std_x = 0
     max_x = 0
     max_y = 0
     for img_name in data:
         im_bw = cv2.imread(img_name,0) #Grayscale conversion
-        height, width = im_bw.shape
         retval, thresh_gray = cv2.threshold(im_bw, thresh=100, maxval=255, \
                                             type=cv2.THRESH_BINARY_INV)
 
@@ -132,38 +128,30 @@ def CroppedCharAnalysis(data_path):
                 mx_area = area
         x, y, w, h = mx
 
-        roi = thresh_gray[y:y + h, x:x + w]
-        ##Extract average column and row sizes
+        roi = thresh_gray[y:y + h, x:x + w]#Keep image of largest contour object
+
         height,width = roi.shape
         img_size_all_y += height
         img_size_all_x += width
-        std_y += abs(height - (img_size_all_y / len(data)))
-        std_x += abs(width - (img_size_all_x / len(data)))
+        #get max dimnesiosn before contour analysis
         if height > max_y:
-            max_imgy = img
             max_y = height
         if width > max_x:
-            max_imgx = img
             max_x = width
-    std_y = std_y / len(data)
-    std_x = std_x / len(data)
+    # get avg dimnesiosn before contour analysis
     avg_x = img_size_all_x / len(data)
     avg_y = (img_size_all_y / len(data))
     print('---After cropping analysis---')
     print("Max image dimmension: ", max_x, max_y)
     print("Average image dimension: ", avg_x, avg_y)
-    print("Standard deviation of dimensions: ", std_x, std_y)
-    return avg_x, avg_y, std_x, std_y,max_x,max_y
+    return avg_x, avg_y,max_x,max_y,img_size_all_x,img_size_all_y
 
-def FindLargestContour(data_path,avg_x,avg_y):
+def FindLargestContour(data_path,avg_x,avg_y,max_x,max_y,std_x,std_y):
+    #This function crops and segments the images, removes the outliers, and saves the images in new folders
     data = glob.glob(data_path)
-    image_no = 1
 
-    #use to find std, mean of newly cropped images.
-    img_size_all_x = 0
-    img_size_all_y = 0
-    std_y = 0
-    std_x = 0
+    outliers_number = 0
+    outliers_paths=[]
     for img_name in data:
         im_bw = cv2.imread(img_name,0) #Grayscale conversion
         height, width = im_bw.shape
@@ -184,37 +172,105 @@ def FindLargestContour(data_path,avg_x,avg_y):
                 mx_area = area
         x, y, w, h = mx
 
+        roi = thresh_gray[y:y + h, x:x + w]
+        height,width= roi.shape
+        #Keep data path to not save outlier in the future
+
+        if (height >= avg_y +3*std_y)  and (width >= avg_x + 3 *std_x):
+            outliers_number+=1
+            outliers_paths.append(img_name)
+
+    print('Outlier number is :',outliers_number)
+    #print(outliers_paths)
+
+    #extract max dimensions after outlier removal for padding
+    max_x = 0
+    max_y = 0
+    for img_name in data:
+        if img_name not in outliers_paths:
+            im_bw = cv2.imread(img_name, 0)  # Grayscale conversion
+            height, width = im_bw.shape
+            retval, thresh_gray = cv2.threshold(im_bw, thresh=100, maxval=255, \
+                                                type=cv2.THRESH_BINARY_INV)
+
+            contours, image = cv2.findContours(thresh_gray, cv2.RETR_LIST, \
+                                               cv2.CHAIN_APPROX_SIMPLE)
+
+            # Find object with the biggest bounding box
+            mx = (0, 0, 0, 0)  # biggest bounding box so far
+            mx_area = 0
+            for cont in contours:
+                x, y, w, h = cv2.boundingRect(cont)
+                area = w * h
+                if area > mx_area:
+                    mx = x, y, w, h
+                    mx_area = area
+            x, y, w, h = mx
+
+            roi = thresh_gray[y:y + h, x:x + w]
+            height, width = roi.shape
+            #extract max dimensions for padding
+            if height > max_y:
+                max_y = height
+            if width > max_x:
+                max_x = width
+    print(max_x,max_y,'new max dimensions after outlier removal.')
+        #f, axarr = plt.subplots(2, 2)
+        #axarr[0,0].imshow(thresh_gray,cmap='gray')
+        #axarr[0,1].imshow(roi,cmap='gray')
+        #plt.show()
+    #now pad according to max dimensions and save
+
+
+
+def findstd(data_path,avg_x,avg_y,img_size_all_x,img_size_all_y):
+    data = glob.glob(data_path)
+    std_y = 0
+    std_x = 0
+    for img_name in data:
+        im_bw = cv2.imread(img_name, 0)  # Grayscale conversion
+        retval, thresh_gray = cv2.threshold(im_bw, thresh=100, maxval=255,
+                                            type = cv2.THRESH_BINARY_INV)
+
+        contours, image = cv2.findContours(thresh_gray, cv2.RETR_LIST,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+        # Find object with the biggest bounding box
+        mx = (0, 0, 0, 0)  # biggest bounding box so far
+        mx_area = 0
+        for cont in contours:
+            x, y, w, h = cv2.boundingRect(cont)
+            area = w * h
+            if area > mx_area:
+                mx = x, y, w, h
+                mx_area = area
+        x, y, w, h = mx
 
         roi = thresh_gray[y:y + h, x:x + w]
 
-      # roiresize = resize(roi,(int(avg_x),int(avg_y)))
-        #roipadded =
+        height, width = roi.shape
+        std_y += abs(height - (img_size_all_y / len(data)))
+        std_x += abs(width - (img_size_all_x / len(data)))
 
+    std_y = std_y / len(data)
+    std_x = std_x / len(data)
 
-
-        f, axarr = plt.subplots(2, 2)
-        axarr[0,0].imshow(roiresize,cmap='gray')
-        axarr[0,1].imshow(roi,cmap='gray')
-        print(roi.shape,'aaa')
-        plt.show()
-
-        #First save for extracting data info
-        #name = 'C:/Users/Panos/Desktop/HandwritingRecognition/HandwritingRecog/data/Cropped/' + str(image_no) + '.PGM'
-        #im = Image.fromarray(roi)
-        #im.save(name, 'PGM')
-        #image_no += 1
-
-
+    print('For cropped pictures std of x and y:')
+    print( std_x, std_y )
+    return std_x,std_y
 
 
 
 #Data investigation before crop
 avg_x,avg_y = data_size_stats("data/monkbrill/*/*.pgm")
 #Get stats for cropped images
-avg_x, avg_y,std_x,std_y,max_x,max_y = CroppedCharAnalysis("data/monkbrill/*/*.pgm")
+avg_x, avg_y,max_x,max_y, img_size_all_x, img_size_all_y = CroppedCharAnalysis("data/monkbrill/*/*.pgm")
 #crop ,resize,save images
-FindLargestContour("data/monkbrill/*/*.pgm",avg_x,avg_y,max_x,max_y)
+std_x,std_y = findstd("data/monkbrill/*/*.pgm",avg_x,avg_y,img_size_all_x,img_size_all_y)
 
+
+
+FindLargestContour("data/monkbrill/*/*.pgm",avg_x,avg_y,max_x,max_y,std_x,std_y)
 
 
 
