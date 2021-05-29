57ecbfe41d8985e70a3cb427fef7c3391fa19f86
CharlieAprog <charlie.albietz.uni@gmail.com>
2021-05-29 14:57:16 +0200

    File Seperation of line segmentation

---
+++ Text_Segmentation/plotting.py
@@ -0,0 +1,256 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import cv2
+import os
+from textSegmentation import image_num
+
+
+def handle_saving(plotting_function):
+    def wrapper_function(*args, **kwargs):
+        plotting_function(*args, **kwargs)
+
+        # save_args: save, folder_path, overwrite_path
+        save_args = [x for x in kwargs.values()]
+
+        if not save_args[0]:
+            plt.show()
+        else:
+            if len(save_args) == 3:
+                img_path = f"{save_args[1]}{os.path.sep}{plotting_function.__name__}_img{image_num}.png"
+                if not os.path.exists(
+                        f"{save_args[1]}{os.path.sep}{plotting_function.__name__}_img{image_num}.png"
+                ):
+                    plt.savefig(img_path)
+                    print(
+                        f"Image {os.path.basename(img_path)} has been saved. Overwriting=0."
+                    )
+                elif save_args[2]:
+                    plt.savefig(img_path)
+                    print(
+                        f"Image {os.path.basename(img_path)} has been saved. Overwriting=1."
+                    )
+                else:
+                    print(f"Image path already exists: [{img_path}]")
+            else:
+                # No other cases are needed to be taken into account, as 'save' and 'folder_path' are trivial to be
+                # provided when one wishes to save an image, overwrite is the only thing that may easily be forgotten
+                raise IndexError(
+                    f"Expected number of arguments: 3 (save, folder_path, overwrite) but received only 2"
+                )
+
+    return wrapper_function
+
+
+@handle_saving
+def plotHist(hist,
+             y_threshold,
+             save=False,
+             folder_path=None,
+             overwrite_path=False):
+    fs = 25
+    plt.figure(figsize=(10, 6))
+    plt.plot(hist)
+    plt.axhline(y=y_threshold, color="r", linestyle="-")
+    plt.ylim(0, max(hist) * 1.1)
+    plt.xlabel("Row", fontsize=fs)
+    plt.ylabel("Black pixels", fontsize=fs)
+    plt.title("Binary image black pixel counting result", fontsize=fs)
+    plt.yticks(fontsize=fs - 5)
+    plt.xticks(fontsize=fs - 5)
+    plt.grid()
+
+
+@handle_saving
+def plotHistLinesOnImage(newImage,
+                         midlines,
+                         save=False,
+                         folder_path=None,
+                         overwrite_path=False):
+    plt.figure(figsize=(10, 6))
+    plt.imshow(newImage, cmap="gray")
+    for i in range(len(midlines)):
+        for idx, loc in enumerate(midlines[i]):
+            if idx == 0:
+                plt.axhline(y=loc, color="r", linestyle="-")
+            else:
+                plt.axhline(y=loc, color="b", linestyle="-")
+
+
+@handle_saving
+def plotPathsNextToImage(binary_image,
+                         paths,
+                         save=False,
+                         folder_path=None,
+                         overwrite_path=False):
+    fig, ax = plt.subplots(figsize=(16, 12), ncols=2)
+    for path in paths:
+        path = np.array(path)
+        ax[1].plot((path[:, 1]), path[:, 0])
+    ax[1].axis("off")
+    ax[0].axis("off")
+    ax[1].imshow(binary_image, cmap="gray")
+    ax[0].imshow(binary_image, cmap="gray")
+
+
+@handle_saving
+def plotHoughTransform(hspace,
+                       theta,
+                       dist,
+                       x0,
+                       x1,
+                       origin,
+                       image,
+                       newImage,
+                       save=False,
+                       folder_path=None,
+                       overwrite_path=False):
+    fig, axes = plt.subplots(1, 4, figsize=(15, 6))
+    ax = axes.ravel()
+
+    # Axis 0
+    ax[0].imshow(image, cmap='gray')
+    ax[0].set_title('Input image')
+    ax[0].set_axis_off()
+
+    # Axis 1
+    ax[1].imshow(np.log(1 + hspace),
+                 extent=[
+                     np.rad2deg(theta[-1]),
+                     np.rad2deg(theta[0]), dist[-1], dist[0]
+                 ],
+                 cmap='gray',
+                 aspect=1 / 1.5)
+    ax[1].set_title('Hough transform')
+    ax[1].set_xlabel('Angles (degrees)')
+    ax[1].set_ylabel('Distance (pixels)')
+    ax[1].axis('image')
+
+    # Axis 2
+    ax[2].imshow(image, cmap='gray')
+    ax[2].plot(origin, (x0, x1), '-b')
+    ax[2].set_xlim(origin)
+    ax[2].set_ylim((image.shape[0], 0))
+    ax[2].set_axis_off()
+    ax[2].set_title('Detected lines')
+
+    # Axis 3
+    ax[3].imshow(newImage, cmap='gray')
+
+    plt.tight_layout()
+
+
+def plotSimpleImages(image_list):
+    fig, ax = plt.subplots(nrows=len(image_list), figsize=(5, 6))
+    for index, image in enumerate(image_list):
+        if len(image_list) > 1:
+            ax[index].imshow(image, cmap="gray")
+        else:
+            ax.imshow(image, cmap="gray")
+    plt.show()
+
+
+def plotWordsInLine(line, words):
+    fig, ax = plt.subplots(nrows=len(words), figsize=(5, 8))
+    for index, word in enumerate(words):
+        ax[index].axis("off")
+        ax[index].imshow(word, cmap="gray")
+    plt.show()
+
+
+def plotGrid(images):
+    # distribute bottom row images
+    bottom_images = images[2:]
+    axes = []
+    fig = plt.figure()
+    # ---------- odd amount of images ----------
+    if len(bottom_images) % 2:
+        bottom_first_col = bottom_images[:len(bottom_images) // 2]
+        bottom_second_col = bottom_images[len(bottom_images) // 2:-1]
+        total_col_num = 2 + len(bottom_first_col) + 1
+        axes.append(
+            plt.subplot2grid((total_col_num, 2), (total_col_num - 1, 0),
+                             colspan=1))
+    # ---------- even amount of images ----------
+    else:
+        bottom_first_col = bottom_images[:len(bottom_images) // 2]
+        bottom_second_col = bottom_images[len(bottom_images) // 2:]
+        total_col_num = 2 + len(bottom_first_col)
+    # grid: first two rows
+    for i in range(2):
+        axes.insert(0, (plt.subplot2grid((total_col_num, 2), (i, 0),
+                                         colspan=2,
+                                         rowspan=round(total_col_num * 0.25))))
+    # grid: last row
+    for idx, image in enumerate(bottom_first_col):
+        axes.insert(-2, (plt.subplot2grid(
+            (total_col_num, 2), (2 + idx, 0), colspan=1)))
+    for idx, image in enumerate(bottom_second_col):
+        axes.insert(-2, (plt.subplot2grid(
+            (total_col_num, 2), (2 + idx, 1), colspan=1)))
+    # plot all rows
+    print(len(axes), len(images))
+    for idx, ax in enumerate(axes):
+        ax.plot(images[idx])
+    plt.show()
+
+
+def plotConnectedComponentLabel(path):
+    """
+    Plots the connected components of an image file.
+    In our system it should take a binary image (black background, white foreground) as 'path'.
+    """
+    # Getting the input image
+    img = cv2.imread(path, 0)
+    # Converting those pixels with values 1-127 to 0 and others to 1
+    img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
+    print(type(img))
+    print(type(img[0]), img[0])
+    # Applying cv2.connectedComponents()
+    num_labels, labels = cv2.connectedComponents(img)
+
+    # Map component labels to hue val, 0-179 is the hue range in OpenCV
+    label_hue = np.uint8(179 * labels / np.max(labels))
+    blank_ch = 255 * np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+    # Converting cvt to BGR
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+    # set bg label to black
+    labeled_img[label_hue == 0] = 0
+
+    # Showing Original Image
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.axis("off")
+    plt.title("Orginal Image")
+    plt.show()
+
+    # Showing Image after Component Labeling
+    plt.imshow(cv2.cvtColor(labeled_img, cv2.COLOR_BGR2RGB))
+    plt.axis('off')
+    plt.title("Image after Component Labeling")
+    plt.show()
+
+
+def plotConnectedComponentBoundingBoxes(image, rectangle_boundaries):
+    plt.imshow(image, cmap="gray")
+    ax = plt.gca()
+    for idx, box in enumerate(rectangle_boundaries):
+        if idx == 0:
+            print(box)
+        y_min = box[0][1]
+        y_max = box[0][0]
+        x_min = box[1][0]
+        x_max = box[1][1]
+        width = x_max - x_min
+        height = y_max - y_min
+        ax.add_patch(
+            Rectangle((x_min, y_min),
+                      width,
+                      height,
+                      linewidth=1,
+                      edgecolor='r',
+                      facecolor='none'))
+    plt.title("Image with bounding boxes")
+    plt.show()
---
+++ Text_Segmentation/wordSegmentation.py
@@ -0,0 +1,105 @@
+from skimage.filters import threshold_otsu
+import numpy as np
+
+def extract_line_from_image(image, upper_line, lower_line):
+    upper_boundary = np.min(upper_line[:, 0])
+    lower_boundary = np.max(lower_line[:, 0])
+    img_copy = np.copy(image)
+    r, c = img_copy.shape
+    x_axis = 0
+    y_axis = 1
+    for step in upper_line:
+        img_copy[0:step[x_axis], step[y_axis]] = 0
+    for step in lower_line:
+        img_copy[step[x_axis]:r, step[y_axis]] = 0
+    return img_copy[upper_boundary:lower_boundary, :]
+
+
+def trim_line(line):
+    thresh = threshold_otsu(line)
+    line = line > thresh
+    vertical_projection = np.sum(line, axis=0)
+    b1 = 0
+    b2 = 0
+    beginning = 0
+    end = 0
+    temp1 = 0
+    temp2 = 0
+
+    for idx in range(len(vertical_projection)):
+        if beginning == 0:
+            if vertical_projection[idx] == 0:  # white
+                if b1 <= 10:
+                    temp1 = 0
+                    b1 = 0
+            elif vertical_projection[idx] != 0:  # black
+                if b1 == 0:  # start of black
+                    temp1 = idx - 5 if idx - 5 > 0 else idx
+                if b1 > 10:
+                    beginning = temp1
+                b1 += 1
+
+        if end == 0:
+            idx2 = len(vertical_projection) - (idx + 1)
+            if vertical_projection[idx2] == 0:  # white
+                if b2 <= 10:
+                    temp2 = 0
+                    b2 = 0
+            elif vertical_projection[idx2] != 0:  # black
+
+                if b2 == 0:  # start of black
+                    temp2 = idx2 + 5 if idx + \
+                        5 < len(vertical_projection) else idx2
+                if b2 > 10:
+                    end = temp2
+                b2 += 1
+
+        if end != 0 and beginning != 0:
+            break
+
+    new_line = line[:, beginning:end]
+    return new_line
+
+
+def segment_words(line, vertical_projection):
+    whitespace_lengths = []
+    whitespace = 0
+
+    # get whitespae lengths
+    for idx in range(5, len(vertical_projection)-4):
+        if vertical_projection[idx] == 0:
+            whitespace = whitespace + 1
+        elif vertical_projection[idx] != 0:
+            if whitespace != 0:
+                whitespace_lengths.append(whitespace)
+            whitespace = 0  # reset whitepsace counter.
+        if idx == len(vertical_projection)-1:
+            whitespace_lengths.append(whitespace)
+
+    print("whitespaces:", whitespace_lengths)
+    avg_white_space_length = np.mean(whitespace_lengths)
+    print("average whitespace lenght:", avg_white_space_length)
+
+    # find index of whitespaces which are actually long spaces using the avg_white_space_length
+    whitespace_length = 0
+    divider_indexes = []
+    divider_indexes.append(0)
+    for index, vp in enumerate(vertical_projection[4:len(vertical_projection) - 5]):
+        if vp == 0:  # white
+            whitespace_length += 1
+        elif vp != 0:  # black
+            if whitespace_length != 0 and whitespace_length > avg_white_space_length:
+                divider_indexes.append(index-int(whitespace_length/2))
+            whitespace_length = 0  # reset it
+    divider_indexes.append(len(vertical_projection) - 1)
+    divider_indexes = np.array(divider_indexes)
+    dividers = np.column_stack((divider_indexes[:-1], divider_indexes[1:]))
+
+    word_sizes = [np.sum(np.sum(line[:, window[0]:window[1]], axis=1))
+                  for window in dividers]
+    print(word_sizes)
+
+    new_dividers = [window for window in dividers if np.sum(
+        np.sum(line[:, window[0]:window[1]], axis=0)) > 200]
+
+    return new_dividers
