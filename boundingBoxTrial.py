a51425a80ea0fd30f11842d179de8151f6d6ac2b
JanRob23 <jan.henner.roberg@gmail.com>
2021-06-06 12:12:47 +0200

    cnn trained in correct input (0 background, 1 character)

--- boundingBoxTrial.py
+++ boundingBoxTrial.py
@@ -67,31 +67,12 @@ for slide in sliding_words[0][0]:
 
 lines, words_in_lines = text_segment(image_num) 
 sliding_words = get_sliding_words(words_in_lines,window_size, shift)
-word_box_images = character_segment(words_in_lines[2][0]) # list of numpy array (images)
-# plotSimpleImages(word_box_images)
-# plotSimpleImages(sliding_words[2][2])
-print(words_in_lines)
-print(len(words_in_lines[0]))
-print(len(words_in_lines[0][0]))
-for line in range(len(words_in_lines[0])):
-    for word in words_in_lines[0]:
-        plotSimpleImages([word])
-        plotSimpleImages([words_in_lines[0][0]])
-        word_boxes_img = character_segment([word])
-        plotSimpleImages(word_boxes_img)
-#word_img = 
-#box_img = 
-#bounding_box_cords = x, y
-for img in word_box_images:
-    label, prob_bounding_box = get_label_probability(img, model)
-    print(idx2name[int(label)])
-    print(label, prob_bounding_box)
-    dim = img.shape
-    print(dim)
-    images = slide_over_word(img, 30, 10)
-    plotSimpleImages(images)
-plotSimpleImages(word_box_images)
+word_box_images = character_segment(words_in_lines[1][2]) # list of numpy array (images)
 
+all_words = [word for line in words_in_lines for word in line]
+print(all_words[0])
+for word in all_words:
+    plotSimpleImages([word])
 
 # plotSimpleImages(sliding_words[2][2])
 #
---
+++ model_sanity_check.py
@@ -0,0 +1,61 @@
+from segmentation_to_recog import *
+
+
+# loop over the val data and call the function plus some other stuff I tried
+path_to_val_data = '/home/jan/PycharmProjects/HandwritingRecog/data/Char_Recog/binarized_monkbrill_split_40x40_morphed/val_no_morph/*/*.png'
+val_data = glob.glob(path_to_val_data)
+cor = 0
+wro = 0
+class ThresholdTransform(object):
+  def __init__(self, thr_255):
+    self.thr = thr_255 / 255.  # input threshold for [0..255] gray level, convert to [0..1]
+
+  def __call__(self, x):
+    return (x < self.thr).to(x.dtype)  # do not change the data type
+bin_transform = transforms.Compose([
+    transforms.ToTensor(),
+    ThresholdTransform(thr_255=250)
+])
+for img_name in val_data:
+    folders = img_name.split('/')
+    label = folders[9]
+    print(label)
+    label_idx = name2idx[label]
+    _, img = boundingboxcrop(img_name)
+    #print(img)
+    if img != []:
+        img = resize_pad(img, 40, 40)
+        img = bin_transform(img)
+        img = torch.reshape(img, [1,1,40,40])
+        #print(img[0][0])
+        out = model(img)
+        pred_label = torch.argmax(out).detach().numpy()
+
+        if pred_label == label_idx:
+            cor += 1
+        else:
+            wro += 1
+print(cor/(cor+wro))
+
+    # img = resize_pad(img, 40, 40)
+    # img_res = np.reshape(img, [1, 1, 40, 40])
+    # img_torch = torch.Tensor(img_res)
+    # out = model(img_torch)
+    # pred_label = torch.argmax(out)
+    # y_true.append(label_idx)
+    # y_pred.append(pred_label)
+    # out = out.detach().numpy()
+    # char_probs = np.exp(out) / (np.exp(out)).sum()
+    # if pred_label != label_idx:
+    #     wrong += 1
+    #
+    #     print(char_probs[0][pred_label])
+    #     if char_probs[0][pred_label] < 0.6:
+    #         x = int(pred_label.numpy())
+    # #         print(label, idx2name[x])
+    # else:
+    #     correct += 1
+# c_mat = confusion_matrix(y_true, y_pred)
+# sn.heatmap(c_mat)
+# print(c_mat)
+# plt.show()
